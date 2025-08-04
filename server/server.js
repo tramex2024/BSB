5f9cdbca5cbb6bfbe0a774cd7806b4a52941fc2f
Sergio Enrique Rojas Medina <tramex2024@gmail.com>
2025-08-04 14:19:02 -0600

    price3

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// Configuración de CORS para permitir solo tu dominio de Vercel
+const corsOptions = {
+  origin: 'https://bsb-lime.vercel.app'
+};
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // Conexión a MongoDB
