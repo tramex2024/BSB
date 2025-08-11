62bc3f26b936468399cc3a4ea1aeb617964be3d8
Sergio Enrique Rojas Medina <tramex2024@gmail.com>
2025-08-11 15:09:26 -0600

    graph4

--- public/js/modules/bot.js
+++ public/js/modules/bot.js
@@ -2,7 +2,7 @@
 
 import { displayLogMessage } from './auth.js';
 import { fetchFromBackend } from './api.js';
-import { TRADE_SYMBOL } from '../main.js';
+import { TRADE_SYMBOL_BITMART } from '../main.js';
 import { BACKEND_URL } from '../main.js'; // Importa la URL del backend
 
 let isRunning = false;
