1dd02d4b1dd748a83822024ea1004c6bb9569489
Sergio Enrique Rojas Medina <tramex2024@gmail.com>
2025-09-07 15:08:50 -0600

    login3

--- public/js/modules/dashboard.js
+++ public/js/modules/dashboard.js
@@ -2,8 +2,7 @@
 
 import { getBalances } from './balance.js';
 import { checkBitMartConnectionAndData } from './network.js';
-import { TRADE_SYMBOL_TV, TRADE_SYMBOL_BITMART, currentChart, intervals } from '../main.js';
-//import { intervals } from '../main.js'; 
+import { intervals } from '../main.js';
 
 export function initializeDashboardView() {
     console.log("Inicializando vista del Dashboard...");
