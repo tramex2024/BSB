18abfb4647fbb76cfe84f5868df10b9689bebf5c
Sergio Enrique Rojas Medina <tramex2024@gmail.com>
2025-09-15 14:21:42 -0600

    front3

--- server/routes/autobotRoutes.js
+++ server/routes/autobotRoutes.js
@@ -85,7 +85,7 @@ router.post('/stop', async (req, res) => {
     }
 });
 
-/ Ruta para actualizar la configuración del bot
+// Ruta para actualizar la configuración del bot
 router.post('/update-config', auth, async (req, res) => {
     try {
         const { config } = req.body;
