f894c0838e7480ce77c5b18f61b8427a44148d95
tramex2024 <tramex2024@gmail.com>
2026-07-02 09:54:07 +0200

    start1

--- server/utils/email.js
+++ server/utils/email.js
@@ -16,7 +16,7 @@ console.log("🔍 EMAIL_PASS Longitud:", process.env.EMAIL_PASS ? process.env.EM
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
-    secure: true, // true para puerto 465     
+    secure: true, 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
