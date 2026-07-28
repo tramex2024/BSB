8c41df01f07c8b98d0c93e1ca9ee6b02c4c1f507
tramex2024 <tramex2024@gmail.com>
2026-07-28 16:04:24 +0200

    start1

--- public/js/modules/chart.js
+++ public/js/modules/chart.js
@@ -14,7 +14,7 @@ export function initializeChart(containerId, symbol) {
     const container = document.getElementById(containerId);
     if (!container) return;
 
-    //const fullSymbol = `BITMART:${symbol}`;
+    const fullSymbol = `BINANCE:${symbol}`;
 
     // [BLINDAJE]: Si el contenedor ya tiene un iframe activo y el símbolo es idéntico, 
     // evitamos destruir el DOM para prevenir cortes en el flujo de precios en tiempo real.
@@ -32,7 +32,7 @@ export function initializeChart(containerId, symbol) {
         tvWidgetInstances[containerId] = fullSymbol;
         new TradingView.widget({
             "autosize": true, 
-            "symbol": "BINANCE:BTCUSDT",
+            "symbol": fullSymbol,
             "interval": savedInterval,
             "timezone": "Etc/UTC",
             "theme": "dark",
